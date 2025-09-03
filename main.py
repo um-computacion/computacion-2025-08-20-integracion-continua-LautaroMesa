33cf6dbd9566bfd39c53e568f2c9d5ef42d86e4b
Lautaro Mesa <la0.mesa@alumno.um.edu.ar>
2025-09-03 11:45:08 +0000

    Aplicacion pylint

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def suma (a, b) :
+"""Este módulo contiene una función para sumar dos números."""
+
+
+def suma(a, b):
+    """Devuelve la suma de dos números."""
     return a + b
-if __name__ == "main":
+
+
+if __name__ == "__main__":
     print(suma(2, 3))
